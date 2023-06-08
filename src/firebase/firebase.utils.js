1ad74e7bd9e3556defbc454600ab0f9a50373608
wickeddeveloper4l <wickeddeveloper4l@gmail.com>
2023-06-08 04:32:02 +0100

    Day owl authentication and pushing to production

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,81 +1,71 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
-import {  getDoc, doc, setDoc } from 'firebase/firestore';
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSQX96hSfkCKOIa09H06wwt9d0hfilJ6U",
-    authDomain: "materioo-dashboard.firebaseapp.com",
-    projectId: "materioo-dashboard",
-    storageBucket: "materioo-dashboard.appspot.com",
-    messagingSenderId: "688063765764",
-    appId: "1:688063765764:web:3baf4f2bbdc94a1c414451",
-    measurementId: "G-QZBDWQFZ10"
-  };
+  apiKey: "AIzaSyDoAIbJZCaOIfA8P-JkvGszIfn3Dv_c6VM",
+  authDomain: "day-owl-ui-kit.firebaseapp.com",
+  projectId: "day-owl-ui-kit",
+  storageBucket: "day-owl-ui-kit.appspot.com",
+  messagingSenderId: "869773007216",
+  appId: "1:869773007216:web:78f63994ad1728f60857b5",
+};
 
-  const app = initializeApp(firebaseConfig)
-  export const auth = getAuth()
-  export const db = getFirestore(app)
-  const provider = new GoogleAuthProvider()
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
 
-  export const googleHandler =  () => {
-    signInWithPopup(auth, provider)
-}
+export const googleHandler = () => {
+  signInWithPopup(auth, provider);
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData)=>{
-    if(!userAuth) return
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
+  const userRef = doc(db, "users", `${userAuth.uid}`);
+  const snapShot = await getDoc(userRef);
 
-           
-            const userRef = doc(db,"users",`${userAuth.uid}`)
-            const snapShot = await getDoc(userRef)
+  if (!snapShot.exists() && additionalData !== undefined) {
+    const { firstName, lastName } = additionalData;
+    const { email, uid } = userAuth;
+    const createdAt = new Date();
 
+    try {
+      await setDoc(userRef, {
+        email,
+        firstName,
+        lastName,
+        createdAt,
+        id: uid,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else if (!snapShot.exists() && additionalData === undefined) {
+    const { email, uid, displayName } = userAuth;
+    const createdAt = new Date();
 
-            if(!snapShot.exists() && additionalData!== undefined){
-                const { firstName, lastName } = additionalData
-                const { email, uid } = userAuth
-                const createdAt = new Date()
-                
-                try {
-                    await setDoc(userRef, {
-                        email,
-                        firstName,
-                        lastName,
-                        createdAt,
-                        id:uid
-                    })
-                } 
-                catch (error) {
-                    console.log(error.message)
-                }
-            }
-
-            else if(!snapShot.exists() && additionalData === undefined){
-                const { email, uid, displayName } = userAuth
-                const createdAt = new Date()
-
-                try {
-                    await setDoc(userRef, {
-                        email,
-                        firstName:displayName,
-                        createdAt,
-                        id:uid
-                    })
-                } 
-                catch (error) {
-                    console.log(error.message)
-                }
-            }
-        return userRef
-    
-}
+    try {
+      await setDoc(userRef, {
+        email,
+        firstName: displayName,
+        createdAt,
+        id: uid,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return userRef;
+};
 
 export const getCurrentUser = async () => {
-    return new Promise((resolve, reject)=>{
-     const unsubcribe = auth.onAuthStateChanged(userAuth => {
-         unsubcribe()
-         resolve(userAuth)
-     }, reject)
-    })
- }
+  return new Promise((resolve, reject) => {
+    const unsubcribe = auth.onAuthStateChanged((userAuth) => {
+      unsubcribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
